3245743cd082cb43a30cc835ed09df798783cba5
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-02-16 12:58:37 -0300

    style: adjusting input margins

--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -10,46 +10,49 @@ export const Container = styled.div`
 `;
 
 export const Messages = styled.div`
+  flex: 1;
   padding: 20px 0;
   display: flex;
   flex-direction: column;
-  max-height: calc(100vh - 46px - 68px);
+  max-height: calc(100vh - 46px - 72px);
   overflow-y: scroll;
+
   ::-webkit-scrollbar {
     width: 8px;
   }
+
   ::-webkit-scrollbar-thumb {
     background-color: var(--tertiary);
     border-radius: 4px;
   }
-  ::-webkit-scrollbar-track {
-    background-color: var(--secondary);
-  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  height: 72px;
+  padding: 4px 16px;
 `;
 
 export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   padding: 0 16px;
+  background-color: var(--chat-input);
+  border-radius: 7px;
 `;
 
 export const Input = styled.input`
-  width: 100%;
+  flex: 1;
   height: 44px;
-  padding: 0 10px 0 57px;
-  border-radius: 7px;
   color: var(--white);
-  background-color: var(--chat-input);
+  background-color: transparent;
   position: relative;
   
   &::placeholder {
     color: var(--gray);
-  }
-  
-  ~ svg {
-    position: relative;
-    top: -50%;
-    left: 14px;
-    transition: 180ms ease-in-out;
   }
 `;
 
